266ce22f526fa06385bf2b123bb26861f9413bc2
wangxingwei <wangxingwei@douyu.tv>
2017-05-03 14:42:29 +0800

    componentWillUnmount触发时机

--- app/js/app.js
+++ app/js/app.js
@@ -1,46 +1,59 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import DevTools from './devtool.js';
+import DevTools from "./devtool.js";
 
-const SoundControl = ({muted}) => {
-    console.log('---------------------Sound Changed--------------------', muted);
-    return <div>你就当我是一个视频吧，音量
-             <span style={ { color: 'red' } }><b>{ muted ? '静音' : '有声' }</b></span>
-           </div>;
+const SoundControl = ({ muted }) => {
+  console.log("---------------------Sound Changed--------------------", muted);
+  return (
+    <div>
+      你就当我是一个视频吧，音量
+      <span style={{ color: "red" }}><b>{muted ? "静音" : "有声"}</b></span>
+    </div>
+  );
+};
+
+class A extends React.PureComponent {
+  componentWillUnmount() {
+    console.log("A will unmount!");
+  }
+
+  render() {
+    return <img src="img/a.jpg" width="500" height="566" />;
+  }
 }
 
-const A = ({visible}) => {
-    return <img src='img/a.jpg' width='500' height='566' style={ { visibility: visible ? 'visible' : 'hidden' } } />
+class B extends React.PureComponent {
+  componentWillUnmount() {
+    console.log("B will unmount!");
+  }
+
+  render() {
+    return <img src="img/b.jpg" width="500" height="566" />;
+  }
 }
 
-const B = ({visible}) => {
-    return <img src='img/b.jpg' width='500' height='566' style={ { visibility: visible ? 'visible' : 'hidden' } } />
-}
+class App extends React.PureComponent {
+  render() {
+    const { isA, isB } = this.props;
 
-class App extends React.Component {
-
-    render() {
-        const {isA, isB} = this.props;
-
-        return <div>
-                 <A visible={ isA } />
-                 <B visible={ isB } />
-                 <SoundControl muted={ isA || isB } />
-                 <DevTools/>
-               </div>
-    }
-
+    return (
+      <div>
+        {isA && <A />}
+        {isB && <B />}
+        <SoundControl muted={isA || isB} />
+        <DevTools />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    const {isA, isB} = state;
-    return {
-        isA,
-        isB
-    }
-}
+  const { isA, isB } = state;
+  return {
+    isA,
+    isB
+  };
+};
 
-export default connect(
-    mapStateToProps,
-)(App);
+export default connect(mapStateToProps)(App);
